8c1fa72c188412d1f46d1d7e1754d878b88a6802
BostonSlettum22 <816661@mystma.org>
2021-02-11 17:57:49 +0000

    test 3 try 1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+
+    total = 1
+
+    for i in range(1,len(w)):
+        total = total + i
+
+    if w[i] == "b":
+        return len(w)
+
+print(countB("boston"))
+
+
+
+def removeLast(w):
+    bazinga = w[len(w)-1]
+
+    for i in range(2,len(w)+1):
+        bazinga = w[len(w)-i] + bazinga 
+
+    return bazinga
+
+print(removeLast("winter"))
+
